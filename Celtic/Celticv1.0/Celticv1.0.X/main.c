b6ea9e793a512ce3e8415591abfeafc732840efc
Arun Kumar <arun.kumar@smartantennatech.com>
2015-12-17 15:55:02 +0000

    Celtic v1.0 coded for EVB02a. Additional options to control 4 SSCs.
    Signed-off-by: Arun Kumar <arun.kumar@smartantennatech.com>

---
+++ Celtic/Celticv1.0/Celticv1.0.X/main.c
@@ -0,0 +1,274 @@
+/**
+ Celtic v 1.0 for EVB02a. Code to control 2 modules of SSC (4 SSCs in total)
+
+  Description:
+    This header file provides implementations for driver APIs for all modules selected in the GUI.
+    Generation Information :
+        Product Revision  :  MPLAB® Code Configurator - v2.25.2
+        Device            :  PIC16LF1618
+        Driver Version    :  2.00
+    The generated drivers are tested against the following:
+        Compiler          :  XC8 v1.34
+        MPLAB             :  MPLAB X v2.35 or v3.00
+ */
+
+#include "mcc_generated_files/mcc.h"
+
+const uint8_t DacOutput[4] = {43, 72, 86, 100}; // 43=0.3V,72=0.5,86=0.6,100=0.7
+//All are in reverse order for SPI mode transfer
+/*const uint8_t USIDLOWREV[36] = {0x4C, 0x27, 0x40, //PWR Mode: Normal
+                                0x4C, 0xB0, 0x41, //F6-F7 ON
+                                0x4C, 0x30, 0x7F, //F1-F6 ON
+                                0x4C, 0xB0, 0x00, //F7 ON
+                                0x4C, 0x30, 0x01, //F6 ON
+                                0x4C, 0x30, 0x02, //F5 ON
+                                0x4C, 0x30, 0x04, //F4 ON
+                                0x4C, 0x30, 0x08, //F3 ON
+                                0x4C, 0x30, 0x10, //F2 ON
+                                0x4C, 0x30, 0x20, //F1 ON
+                                0x4C, 0x30, 0x40, //F1-F7 OFF
+                                0x4C, 0x30, 0x54}; //State 10 (0101000) [F1 F2 F3 F4 F5 F6 F7]
+const uint8_t USIDHIGHREV[33]= {0x5C, 0x07, 0x40,
+                                0x5C, 0x90, 0x41,
+                                0x5C, 0x10, 0x7F,
+                                0x5C, 0x90, 0x00,
+                                0x5C, 0x10, 0x01,
+                                0x5C, 0x10, 0x02,
+                                0x5C, 0x10, 0x04,
+                                0x5C, 0x10, 0x08,
+                                0x5C, 0x10, 0x10,
+                                0x5C, 0x10, 0x20,
+                                0x5C, 0x10, 0x40};
+const uint8_t USIDLOW1[44] =    {0x01, 0x32, 0xE4, 0x02, //PWR Mode: Normal
+                                0x01, 0x32, 0x0D, 0x82, //F6-F7 ON
+                                0x01, 0x32, 0x0C, 0xFE, //F1-F6 ON
+                                0x01, 0x32, 0x0D, 0x00, //F7 ON
+                                0x01, 0x32, 0x0C, 0x80, //F6 ON
+                                0x01, 0x32, 0x0C, 0x40, //F5 ON
+                                0x01, 0x32, 0x0C, 0x20, //F4 ON
+                                0x01, 0x32, 0x0C, 0x10, //F3 ON
+                                0x01, 0x32, 0x0C, 0x08, //F2 ON
+                                0x01, 0x32, 0x0C, 0x04, //F1 ON
+                                0x01, 0x32, 0x0C, 0x02};//F1-F7 OFF
+const uint8_t USIDHIGH1[44] =   {0x01, 0x3A, 0xE0, 0x02, 
+                                0x01, 0x3A, 0x09, 0x82, 
+                                0x01, 0x3A, 0x08, 0xFE, 
+                                0x01, 0x3A, 0x09, 0x00, 
+                                0x01, 0x3A, 0x08, 0x80, 
+                                0x01, 0x3A, 0x08, 0x40, 
+                                0x01, 0x3A, 0x08, 0x20, 
+                                0x01, 0x3A, 0x08, 0x10, 
+                                0x01, 0x3A, 0x08, 0x08, 
+                                0x01, 0x3A, 0x08, 0x04, 
+                                0x01, 0x3A, 0x08, 0x02};*/
+const   uint8_t SLAVEADD[2] =  {0x06, 0x0E};   //USID LOW,USID HIGH
+const   uint8_t REGADD[2]   =  {0x07, 0x10};   //NORMAL POWER-UP ADDRESS,REGISTER ADDRESS
+const   uint8_t DATA[65]    =  {0x00, 0x80, 0x40, 0xC0, 0x20, 0xA0, 0x60, 0xE0, 0x10, 0x90, 0x50, 0xD0, 0x30, 0xB0, 0x70, 0xF0,        //REVERSED DATA 
+                                0x08, 0x88, 0x48, 0xC8, 0x28, 0xA8, 0x68, 0xE8, 0x18, 0x98, 0x58, 0xD8, 0x38, 0xB8, 0x78, 0xF8, 
+                                0x04, 0x84, 0x44, 0xC4, 0x24, 0xA4, 0x64, 0xE4, 0x14, 0x94, 0x54, 0xD4, 0x34, 0xB4, 0x74, 0xF4, 
+                                0x0C, 0x8C, 0x4C, 0xCC, 0x2C, 0xAC, 0x6C, 0xEC, 0x1C, 0x9C, 0x5C, 0xDC, 0x3C, 0xBC, 0x7C, 0xFC, 
+                                0x06}; //STATE64 (F6&F7 ON)   
+
+uint8_t Byte1, Byte2, Byte3; // First 3 bytes from RFFE
+uint8_t parity1, parity2;
+//uint8_t readDummy;
+
+/*void MIPIDATA (uint8_t i){                // Need to enable SPI module to use the same
+    for (uint8_t j=0; j<4; j++)
+                readDummy = SPI_Exchange8bit(USIDLOW[j]);
+    for (uint8_t j=0; j<4; j++)
+                readDummy = SPI_Exchange8bit(USIDLOW[(i+j)]);
+}*/
+
+/*void MIPITRANSFER(uint8_t k) {
+    SDO1_SetHigh();
+    asm("NOP");
+    SDO1_SetLow();
+    readDummy = USIDLOWREV[0];
+    readDummy = readDummy >> 1;
+    for (uint8_t j = 0; j < 7; j++) {
+        SCK_SetHigh();
+        SDO1_LAT = readDummy & 0x01;
+        asm("NOP");
+        SCK_SetLow();
+        readDummy = readDummy >> 1;
+    }
+    for (uint8_t i = 1; i < 3; i++) {
+        readDummy = USIDLOWREV[i];
+        for (uint8_t j = 0; j < 8; j++) {
+            SCK_SetHigh();
+            SDO1_LAT = readDummy & 0x01;
+            asm("NOP");
+            SCK_SetLow();
+            readDummy = readDummy >> 1;
+        }
+    }
+    asm("NOP");
+    asm("NOP");
+    SDO1_SetHigh();
+    asm("NOP");
+    SDO1_SetLow();
+    readDummy = USIDLOWREV[k];
+    readDummy = readDummy >> 1;
+    for (uint8_t j = 0; j < 7; j++) {
+        SCK_SetHigh();
+        SDO1_LAT = readDummy & 0x01;
+        asm("NOP");
+        SCK_SetLow();
+        readDummy = readDummy >> 1;
+    }
+    for (uint8_t i = 1; i < 3; i++) {
+        readDummy = USIDLOWREV[(k + i)];
+        for (uint8_t j = 0; j < 8; j++) {
+            SCK_SetHigh();
+            SDO1_LAT = readDummy & 0x01;
+            asm("NOP");
+            SCK_SetLow();
+            readDummy = readDummy >> 1;
+        }
+    }
+}*/
+
+void shiftRegister (uint8_t Dummy1, uint8_t Dummy2, uint8_t y){
+    for (uint8_t m=0; m<y; m++){
+        SCK_SetHigh();
+        SDO1_LAT = Dummy1 & 0x01;
+        SDO2_LAT = Dummy2 & 0x01;
+        asm("NOP");
+        SCK_SetLow();
+        Dummy1 = Dummy1 >> 1;
+        Dummy2 = Dummy2 >> 1;        
+    }
+}
+
+uint8_t paritycheck (uint8_t Dummy){
+    bool parity = 0;
+    while (Dummy)           //while (Dummy!=0)
+    {
+        parity = !parity;
+        Dummy = Dummy & (Dummy - 1);
+    }        
+    if (parity) return 0;
+    else        return 1;
+}
+
+void MIPI (uint8_t x, uint8_t y){
+    parity1 = paritycheck(DATA[x]);
+    parity2 = paritycheck(DATA[y]);
+    for (uint8_t i=0; i<2; i++){
+        for (uint8_t j=0; j<2; j++){            
+            SDO1_SetHigh();
+            SDO2_SetHigh();
+            asm("NOP");
+            SDO1_SetLow();
+            SDO2_SetLow();
+            shiftRegister (SLAVEADD[i],SLAVEADD[i],4);
+            shiftRegister (0x02,0x02,3);
+            shiftRegister (REGADD[j],REGADD[j],5);
+            if (i==0)   shiftRegister (0x01,0x01,1);     //For USID = 0110, Parity = 1
+            else        shiftRegister (0x00,0x00,1);     //For USID = 0111, Parity = 0
+            if (j==0)   shiftRegister (0x00,0x00,8);
+            else        shiftRegister (DATA[x],DATA[y],8);
+            shiftRegister (parity1,parity2,2);          //Parity + Bus Park
+            asm("NOP");
+            asm("NOP");
+        }
+    }
+}
+
+void main(void) {
+    // initialize the device
+    SYSTEM_Initialize();
+    DAC1_Initialize();
+    PIN_MANAGER_Initialize();
+    //    SPI_Initialize();
+    DAC1_SetOutput(0);
+    CTLA_SetLow(); //RF1 ON
+    CTLB_SetLow();
+    MAIN_NIC_LDO_EN_SetLow(); //0: OFF / MCM sleep mode
+    AUX_NIC_LDO_EN_SetLow();
+    while (1) {
+        while ((PORTC & 0xC0) != 0xC0) {
+        }
+        Byte1 = PORTC; //Sequence starts for Bits 7&6=1&1
+        while ((PORTC & 0xC0) != 0x40) {
+        }
+        Byte2 = PORTC & 0x3F; //Sequence when 7&6 bits toggles (0 1)
+        while ((PORTC & 0xC0) != 0x80) {
+        }
+        Byte3 = PORTC & 0x3F; //Sequence when 7&6 bits toggles (1 0)
+        if (((Byte1 & 0x10) == 0x10) && ((Byte1 & 0x0C) != 0x00)) //ANT_SEL=01/11 & TRX_SEL>0 (DAC Stage)
+            DAC1_SetOutput(DacOutput[(Byte1 & 0x03)]); //DAC Output
+        else
+            DAC1_SetOutput(0);
+        // Main NIC LDO & Aux NIC LDO Power Enable Sequence
+        if ((Byte1 & 0x3C) == 0x20) {
+            MAIN_NIC_LDO_EN_SetLow(); //Main_NIC_LDO is Low
+            AUX_NIC_LDO_EN_SetHigh(); //Aux_NIC_LDO is High        
+        } else {
+            if (((Byte1 & 0x30) != 0x00) && ((Byte1 & 0x0C) != 0x00)) {
+                if ((Byte2 == 5) || (Byte2 == 8) || (Byte2 == 12) || (Byte2 == 13) || (Byte2 == 20) || (Byte2 == 26) || (Byte2 == 29) || (Byte3 == 5) || (Byte3 == 8) || (Byte3 == 12) || (Byte3 == 13) || (Byte3 == 20) || (Byte3 == 26) || (Byte3 == 29)) {
+                    MAIN_NIC_LDO_EN_SetHigh(); //3: Two antennas selected
+                    AUX_NIC_LDO_EN_SetHigh();
+                } else {
+                    MAIN_NIC_LDO_EN_SetLow(); //2: Aux antenna selected
+                    AUX_NIC_LDO_EN_SetHigh();
+                }
+            } else {
+                MAIN_NIC_LDO_EN_SetLow(); //0: OFF / MCM sleep mode
+                AUX_NIC_LDO_EN_SetLow();
+            }
+        }
+        // RF Switch Selection Sequence
+        if (Byte2 == 3 && Byte3 == 7) {
+            CTLA_SetLow(); // RF3 is ON
+            CTLB_SetHigh();
+        } else {
+            if ((Byte2 == 7 && Byte3 == 0) || (Byte2 == 30 && Byte3 == 0) || (Byte2 == 41 && Byte3 == 0) || (Byte2 == 5 && Byte3 == 30) || (Byte2 == 7 && Byte3 == 20) || (Byte2 == 12 && Byte3 == 30) || (Byte2 == 41 && Byte3 == 41)) {
+                CTLA_SetHigh(); // RF2 is ON
+                CTLB_SetLow();
+            } else {
+                CTLA_SetLow(); // RF1 is ON
+                CTLB_SetLow();
+            }
+        }
+        //    MIPIDATA (4);
+        //    MIPIDATA (8);
+        // MIPI Clock and Data Generation
+        if ((Byte2 == 5) && (Byte3 == 0)) {
+            MIPI(10,64);
+//            MIPITRANSFER(33);
+//            MIPITRANSFER(3);
+        } else {
+            if ((Byte2 == 5) || (Byte3 == 5)) {
+//                MIPITRANSFER(3);
+            } else {
+                if ((Byte2 == 8) || (Byte3 == 8)) {
+//                    MIPITRANSFER(6);
+                } else {
+                    if ((Byte2 == 12) || (Byte3 == 12)) {
+//                        MIPITRANSFER(9);
+                    } else {
+                        if ((Byte2 == 13) || (Byte3 == 13)) {
+//                            MIPITRANSFER(12);
+                        } else {
+                            if ((Byte2 == 20) || (Byte3 == 20)) {
+//                                MIPITRANSFER(15);
+                            } else {
+                                if ((Byte2 == 26) || (Byte3 == 26)) {
+//                                    MIPITRANSFER(18);
+                                } else {
+                                    if ((Byte2 == 29) || (Byte3 == 29)) {
+//                                        MIPITRANSFER(21);
+                                    } else {
+//                                        MIPITRANSFER(30);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Celtic/Celticv1.0/Celticv1.0.X/mcc_generated_files/pin_manager.h
@@ -0,0 +1,314 @@
+/**
+  @Generated Pin Manager Header File
+
+  @Company:
+    Microchip Technology Inc.
+
+  @File Name:
+    pin_manager.h
+
+  @Summary:
+    This is the Pin Manager file generated using MPLAB® Code Configurator
+
+  @Description:
+    This header file provides implementations for pin APIs for all pins selected in the GUI.
+    Generation Information :
+        Product Revision  :  MPLAB® Code Configurator - v2.25.2
+        Device            :  PIC16LF1618
+        Version           :  1.01
+    The generated drivers are tested against the following:
+        Compiler          :  XC8 v1.34
+        MPLAB             :  MPLAB X v2.35 or v3.00
+ */
+
+/*
+Copyright (c) 2013 - 2015 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+ */
+
+#ifndef PIN_MANAGER_H
+#define PIN_MANAGER_H
+
+#define INPUT   1
+#define OUTPUT  0
+
+#define HIGH    1
+#define LOW     0
+
+#define ANALOG      1
+#define DIGITAL     0
+
+#define PULL_UP_ENABLED      1
+#define PULL_UP_DISABLED     0
+
+// get/set DAC1OUT aliases
+#define DAC1OUT_TRIS               TRISA0
+#define DAC1OUT_LAT                LATA0
+#define DAC1OUT_PORT               RA0
+#define DAC1OUT_ANS                ANSA0
+#define DAC1OUT_SetHigh()    do { LATA0 = 1; } while(0)
+#define DAC1OUT_SetLow()   do { LATA0 = 0; } while(0)
+#define DAC1OUT_Toggle()   do { LATA0 = ~LATA0; } while(0)
+#define DAC1OUT_GetValue()         RA0
+#define DAC1OUT_SetDigitalInput()    do { TRISA0 = 1; } while(0)
+#define DAC1OUT_SetDigitalOutput()   do { TRISA0 = 0; } while(0)
+
+#define DAC1OUT_SetAnalogMode()   do { ANSA0 = 1; } while(0)
+#define DAC1OUT_SetDigitalMode()   do { ANSA0 = 0; } while(0)
+// get/set CTLA aliases
+#define CTLA_TRIS               TRISA1
+#define CTLA_LAT                LATA1
+#define CTLA_PORT               RA1
+#define CTLA_ANS                ANSA1
+#define CTLA_SetHigh()    do { LATA1 = 1; } while(0)
+#define CTLA_SetLow()   do { LATA1 = 0; } while(0)
+#define CTLA_Toggle()   do { LATA1 = ~LATA1; } while(0)
+#define CTLA_GetValue()         RA1
+#define CTLA_SetDigitalInput()    do { TRISA1 = 1; } while(0)
+#define CTLA_SetDigitalOutput()   do { TRISA1 = 0; } while(0)
+
+#define CTLA_SetAnalogMode()   do { ANSA1 = 1; } while(0)
+#define CTLA_SetDigitalMode()   do { ANSA1 = 0; } while(0)
+// get/set MAIN_NIC_LDO_EN aliases
+#define MAIN_NIC_LDO_EN_TRIS               TRISA2
+#define MAIN_NIC_LDO_EN_LAT                LATA2
+#define MAIN_NIC_LDO_EN_PORT               RA2
+#define MAIN_NIC_LDO_EN_ANS                ANSA2
+#define MAIN_NIC_LDO_EN_SetHigh()    do { LATA2 = 1; } while(0)
+#define MAIN_NIC_LDO_EN_SetLow()   do { LATA2 = 0; } while(0)
+#define MAIN_NIC_LDO_EN_Toggle()   do { LATA2 = ~LATA2; } while(0)
+#define MAIN_NIC_LDO_EN_GetValue()         RA2
+#define MAIN_NIC_LDO_EN_SetDigitalInput()    do { TRISA2 = 1; } while(0)
+#define MAIN_NIC_LDO_EN_SetDigitalOutput()   do { TRISA2 = 0; } while(0)
+
+#define MAIN_NIC_LDO_EN_SetAnalogMode()   do { ANSA2 = 1; } while(0)
+#define MAIN_NIC_LDO_EN_SetDigitalMode()   do { ANSA2 = 0; } while(0)
+// get/set CTLB aliases
+#define CTLB_TRIS               TRISA4
+#define CTLB_LAT                LATA4
+#define CTLB_PORT               RA4
+#define CTLB_ANS                ANSA4
+#define CTLB_SetHigh()    do { LATA4 = 1; } while(0)
+#define CTLB_SetLow()   do { LATA4 = 0; } while(0)
+#define CTLB_Toggle()   do { LATA4 = ~LATA4; } while(0)
+#define CTLB_GetValue()         RA4
+#define CTLB_SetDigitalInput()    do { TRISA4 = 1; } while(0)
+#define CTLB_SetDigitalOutput()   do { TRISA4 = 0; } while(0)
+
+#define CTLB_SetAnalogMode()   do { ANSA4 = 1; } while(0)
+#define CTLB_SetDigitalMode()   do { ANSA4 = 0; } while(0)
+// get/set AUX_NIC_LDO_EN aliases
+#define AUX_NIC_LDO_EN_TRIS               TRISB4
+#define AUX_NIC_LDO_EN_LAT                LATB4
+#define AUX_NIC_LDO_EN_PORT               RB4
+#define AUX_NIC_LDO_EN_ANS                ANSB4
+#define AUX_NIC_LDO_EN_SetHigh()    do { LATB4 = 1; } while(0)
+#define AUX_NIC_LDO_EN_SetLow()   do { LATB4 = 0; } while(0)
+#define AUX_NIC_LDO_EN_Toggle()   do { LATB4 = ~LATB4; } while(0)
+#define AUX_NIC_LDO_EN_GetValue()         RB4
+#define AUX_NIC_LDO_EN_SetDigitalInput()    do { TRISB4 = 1; } while(0)
+#define AUX_NIC_LDO_EN_SetDigitalOutput()   do { TRISB4 = 0; } while(0)
+
+#define AUX_NIC_LDO_EN_SetAnalogMode()   do { ANSB4 = 1; } while(0)
+#define AUX_NIC_LDO_EN_SetDigitalMode()   do { ANSB4 = 0; } while(0)
+// get/set SDO2 aliases
+#define SDO2_TRIS               TRISB5
+#define SDO2_LAT                LATB5
+#define SDO2_PORT               RB5
+#define SDO2_ANS                ANSB5
+#define SDO2_SetHigh()    do { LATB5 = 1; } while(0)
+#define SDO2_SetLow()   do { LATB5 = 0; } while(0)
+#define SDO2_Toggle()   do { LATB5 = ~LATB5; } while(0)
+#define SDO2_GetValue()         RB5
+#define SDO2_SetDigitalInput()    do { TRISB5 = 1; } while(0)
+#define SDO2_SetDigitalOutput()   do { TRISB5 = 0; } while(0)
+
+#define SDO2_SetAnalogMode()   do { ANSB5 = 1; } while(0)
+#define SDO2_SetDigitalMode()   do { ANSB5 = 0; } while(0)
+// get/set SCK aliases
+#define SCK_TRIS               TRISB6
+#define SCK_LAT                LATB6
+#define SCK_PORT               RB6
+#define SCK_WPU                WPUB6
+#define SCK_SetHigh()    do { LATB6 = 1; } while(0)
+#define SCK_SetLow()   do { LATB6 = 0; } while(0)
+#define SCK_Toggle()   do { LATB6 = ~LATB6; } while(0)
+#define SCK_GetValue()         RB6
+#define SCK_SetDigitalInput()    do { TRISB6 = 1; } while(0)
+#define SCK_SetDigitalOutput()   do { TRISB6 = 0; } while(0)
+
+#define SCK_SetPullup()    do { WPUB6 = 1; } while(0)
+#define SCK_ResetPullup()   do { WPUB6 = 0; } while(0)
+// get/set SDO1 aliases
+#define SDO1_TRIS               TRISB7
+#define SDO1_LAT                LATB7
+#define SDO1_PORT               RB7
+#define SDO1_WPU                WPUB7
+#define SDO1_SetHigh()    do { LATB7 = 1; } while(0)
+#define SDO1_SetLow()   do { LATB7 = 0; } while(0)
+#define SDO1_Toggle()   do { LATB7 = ~LATB7; } while(0)
+#define SDO1_GetValue()         RB7
+#define SDO1_SetDigitalInput()    do { TRISB7 = 1; } while(0)
+#define SDO1_SetDigitalOutput()   do { TRISB7 = 0; } while(0)
+
+#define SDO1_SetPullup()    do { WPUB7 = 1; } while(0)
+#define SDO1_ResetPullup()   do { WPUB7 = 0; } while(0)
+// get/set IO_RC0 aliases
+#define IO_RC0_TRIS               TRISC0
+#define IO_RC0_LAT                LATC0
+#define IO_RC0_PORT               RC0
+#define IO_RC0_ANS                ANSC0
+#define IO_RC0_SetHigh()    do { LATC0 = 1; } while(0)
+#define IO_RC0_SetLow()   do { LATC0 = 0; } while(0)
+#define IO_RC0_Toggle()   do { LATC0 = ~LATC0; } while(0)
+#define IO_RC0_GetValue()         RC0
+#define IO_RC0_SetDigitalInput()    do { TRISC0 = 1; } while(0)
+#define IO_RC0_SetDigitalOutput()   do { TRISC0 = 0; } while(0)
+
+#define IO_RC0_SetAnalogMode()   do { ANSC0 = 1; } while(0)
+#define IO_RC0_SetDigitalMode()   do { ANSC0 = 0; } while(0)
+// get/set IO_RC1 aliases
+#define IO_RC1_TRIS               TRISC1
+#define IO_RC1_LAT                LATC1
+#define IO_RC1_PORT               RC1
+#define IO_RC1_ANS                ANSC1
+#define IO_RC1_SetHigh()    do { LATC1 = 1; } while(0)
+#define IO_RC1_SetLow()   do { LATC1 = 0; } while(0)
+#define IO_RC1_Toggle()   do { LATC1 = ~LATC1; } while(0)
+#define IO_RC1_GetValue()         RC1
+#define IO_RC1_SetDigitalInput()    do { TRISC1 = 1; } while(0)
+#define IO_RC1_SetDigitalOutput()   do { TRISC1 = 0; } while(0)
+
+#define IO_RC1_SetAnalogMode()   do { ANSC1 = 1; } while(0)
+#define IO_RC1_SetDigitalMode()   do { ANSC1 = 0; } while(0)
+// get/set IO_RC2 aliases
+#define IO_RC2_TRIS               TRISC2
+#define IO_RC2_LAT                LATC2
+#define IO_RC2_PORT               RC2
+#define IO_RC2_ANS                ANSC2
+#define IO_RC2_SetHigh()    do { LATC2 = 1; } while(0)
+#define IO_RC2_SetLow()   do { LATC2 = 0; } while(0)
+#define IO_RC2_Toggle()   do { LATC2 = ~LATC2; } while(0)
+#define IO_RC2_GetValue()         RC2
+#define IO_RC2_SetDigitalInput()    do { TRISC2 = 1; } while(0)
+#define IO_RC2_SetDigitalOutput()   do { TRISC2 = 0; } while(0)
+
+#define IO_RC2_SetAnalogMode()   do { ANSC2 = 1; } while(0)
+#define IO_RC2_SetDigitalMode()   do { ANSC2 = 0; } while(0)
+// get/set IO_RC3 aliases
+#define IO_RC3_TRIS               TRISC3
+#define IO_RC3_LAT                LATC3
+#define IO_RC3_PORT               RC3
+#define IO_RC3_ANS                ANSC3
+#define IO_RC3_SetHigh()    do { LATC3 = 1; } while(0)
+#define IO_RC3_SetLow()   do { LATC3 = 0; } while(0)
+#define IO_RC3_Toggle()   do { LATC3 = ~LATC3; } while(0)
+#define IO_RC3_GetValue()         RC3
+#define IO_RC3_SetDigitalInput()    do { TRISC3 = 1; } while(0)
+#define IO_RC3_SetDigitalOutput()   do { TRISC3 = 0; } while(0)
+
+#define IO_RC3_SetAnalogMode()   do { ANSC3 = 1; } while(0)
+#define IO_RC3_SetDigitalMode()   do { ANSC3 = 0; } while(0)
+// get/set IO_RC4 aliases
+#define IO_RC4_TRIS               TRISC4
+#define IO_RC4_LAT                LATC4
+#define IO_RC4_PORT               RC4
+#define IO_RC4_WPU                WPUC4
+#define IO_RC4_SetHigh()    do { LATC4 = 1; } while(0)
+#define IO_RC4_SetLow()   do { LATC4 = 0; } while(0)
+#define IO_RC4_Toggle()   do { LATC4 = ~LATC4; } while(0)
+#define IO_RC4_GetValue()         RC4
+#define IO_RC4_SetDigitalInput()    do { TRISC4 = 1; } while(0)
+#define IO_RC4_SetDigitalOutput()   do { TRISC4 = 0; } while(0)
+
+#define IO_RC4_SetPullup()    do { WPUC4 = 1; } while(0)
+#define IO_RC4_ResetPullup()   do { WPUC4 = 0; } while(0)
+// get/set IO_RC5 aliases
+#define IO_RC5_TRIS               TRISC5
+#define IO_RC5_LAT                LATC5
+#define IO_RC5_PORT               RC5
+#define IO_RC5_WPU                WPUC5
+#define IO_RC5_SetHigh()    do { LATC5 = 1; } while(0)
+#define IO_RC5_SetLow()   do { LATC5 = 0; } while(0)
+#define IO_RC5_Toggle()   do { LATC5 = ~LATC5; } while(0)
+#define IO_RC5_GetValue()         RC5
+#define IO_RC5_SetDigitalInput()    do { TRISC5 = 1; } while(0)
+#define IO_RC5_SetDigitalOutput()   do { TRISC5 = 0; } while(0)
+
+#define IO_RC5_SetPullup()    do { WPUC5 = 1; } while(0)
+#define IO_RC5_ResetPullup()   do { WPUC5 = 0; } while(0)
+// get/set IO_RC6 aliases
+#define IO_RC6_TRIS               TRISC6
+#define IO_RC6_LAT                LATC6
+#define IO_RC6_PORT               RC6
+#define IO_RC6_ANS                ANSC6
+#define IO_RC6_SetHigh()    do { LATC6 = 1; } while(0)
+#define IO_RC6_SetLow()   do { LATC6 = 0; } while(0)
+#define IO_RC6_Toggle()   do { LATC6 = ~LATC6; } while(0)
+#define IO_RC6_GetValue()         RC6
+#define IO_RC6_SetDigitalInput()    do { TRISC6 = 1; } while(0)
+#define IO_RC6_SetDigitalOutput()   do { TRISC6 = 0; } while(0)
+
+#define IO_RC6_SetAnalogMode()   do { ANSC6 = 1; } while(0)
+#define IO_RC6_SetDigitalMode()   do { ANSC6 = 0; } while(0)
+// get/set IO_RC7 aliases
+#define IO_RC7_TRIS               TRISC7
+#define IO_RC7_LAT                LATC7
+#define IO_RC7_PORT               RC7
+#define IO_RC7_ANS                ANSC7
+#define IO_RC7_SetHigh()    do { LATC7 = 1; } while(0)
+#define IO_RC7_SetLow()   do { LATC7 = 0; } while(0)
+#define IO_RC7_Toggle()   do { LATC7 = ~LATC7; } while(0)
+#define IO_RC7_GetValue()         RC7
+#define IO_RC7_SetDigitalInput()    do { TRISC7 = 1; } while(0)
+#define IO_RC7_SetDigitalOutput()   do { TRISC7 = 0; } while(0)
+
+#define IO_RC7_SetAnalogMode()   do { ANSC7 = 1; } while(0)
+#define IO_RC7_SetDigitalMode()   do { ANSC7 = 0; } while(0)
+
+/**
+ * @Param
+    none
+ * @Returns
+    none
+ * @Description
+    GPIO and peripheral I/O initialization
+ * @Example
+    PIN_MANAGER_Initialize();
+ */
+void PIN_MANAGER_Initialize(void);
+
+/**
+ * @Param
+    none
+ * @Returns
+    none
+ * @Description
+    Interrupt on Change Handling routine
+ * @Example
+    PIN_MANAGER_IOC();
+ */
+void PIN_MANAGER_IOC(void);
+
+#endif // PIN_MANAGER_H
+/**
+ End of File
+ */

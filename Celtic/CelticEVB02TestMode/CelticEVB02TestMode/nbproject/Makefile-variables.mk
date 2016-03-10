537706d06230f044a897235b2285d1dd32cf718e
Arun Kumar <arun.kumar@smartantennatech.com>
2016-03-10 15:09:41 +0000

    Test Mode created to study the timing diagram for each stages

---
+++ Celtic/CelticEVB02TestMode/CelticEVB02TestMode/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=CelticEVB02TestMode.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/CelticEVB02TestMode.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=celticevb02testmode.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/celticevb02testmode.tar

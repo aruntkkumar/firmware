a4f6851ad989bc9bfa1b0429a7153006484f232a
Arun Kumar <arun.kumar@smartantennatech.com>
2016-09-15 11:32:01 +0100

    Created Firmware for EVB04 Engineering mode running at 4MHz
    Signed-off-by: Arun Kumar <arun.kumar@smartantennatech.com>

---
+++ Celtic/CelticEVB04EngMode4M/CelticEVB04EngMode4M/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=CelticEVB04EngMode4M.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/CelticEVB04EngMode4M.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=celticevb04engmode4m.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/celticevb04engmode4m.tar
